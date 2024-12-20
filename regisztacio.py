e858143c592c240c02b6d5a92292c88338163da1
kkszki <144881913+kkszki@users.noreply.github.com>
2024-12-20 14:46:30 +0100

    Kezdés

---
+++ regisztacio.py
@@ -0,0 +1,13 @@
+allapot="W"
+mas=True
+while allapot!="" and mas:
+    def regisztracio():
+        user=input()
+        pw=input()
+        with open('regisztralt.txt', 'a', encoding='utf-8') as celfajl:
+            print(f'{user}, {pw}', file=celfajl)  
+    
+
+    allapot=input("Kérem írja be, hogy mit szeretne regisztrálni vagy bejelentkezni: ")
+    if allapot =="r":
+        regisztracio()
