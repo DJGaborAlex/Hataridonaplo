1e709fdb80c9a94095a0365ad48d3f78620ffd1a
kkszki <144881913+kkszki@users.noreply.github.com>
2024-12-22 09:41:02 +0100

    Update regisztacio.py
    ez titkosítja a jelszót és a regisztráláskor is hozzá lehet már adni tevékenységeket addig, amíg gondolja.

--- regisztacio.py
+++ regisztacio.py
@@ -1,11 +1,29 @@
 allapot="W"
 
 while allapot!="":
+    bejelentkezve=False
     def regisztracio():
         user=input()
         pw=input()
+        terve=input("Szeretne most hozzá adni vagy majd később? igen vagy nem: ")
+        if terve=="igen":
+            vege="W"
+            teendo=[]
+            while vege!="nem":
+                tevekenység=input("írd be mit szeretnél:")
+                hatarido=input("mikorra")
+                
+                aktualis=[tevekenység, hatarido]
+                teendo.append(aktualis)
+                vege=input("szeretnél még? :")
+            
+        elif terve=="nem":
+            tevekenység=[]
+            hatarido=[]
+        titok = hashlib.sha256(pw.encode())
+        hexa = titok.hexdigest()
         with open('regisztralt.txt', 'a', encoding='utf-8') as celfajl:
-            print(f'{user}, {pw}', file=celfajl)  
+            print(f'{user}, {hexa}, {teendo}, ', file=celfajl)  
     
 
     allapot=input("Kérem írja be, hogy mit szeretne regisztrálni vagy bejelentkezni: ")
